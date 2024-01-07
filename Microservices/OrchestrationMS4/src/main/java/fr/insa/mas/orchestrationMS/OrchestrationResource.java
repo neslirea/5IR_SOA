2068b90dc901670487240c734f074be689ad4591
Arcanee <powerlucario@gmail.com>
2024-01-07 16:16:07 +0100

    cors orchestration

--- Microservices/OrchestrationMS4/src/main/java/fr/insa/mas/orchestrationMS/OrchestrationResource.java
+++ Microservices/OrchestrationMS4/src/main/java/fr/insa/mas/orchestrationMS/OrchestrationResource.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin
 @RestController
 public class OrchestrationResource {
 	
