395302fc49b08e3c20f2ebdd43e186cdc2d9999a
Arcanee <powerlucario@gmail.com>
2024-01-07 17:04:57 +0100

    final

--- Microservices/MissionManagementMS2/src/main/java/fr/insa/mas/missionManagementMS/Status.java
+++
@@ -1,9 +0,0 @@
-package fr.insa.mas.missionManagementMS;
-
-public enum Status {
-	WaitingForValidator,
-	WaitingForVolunteer,
-	InProgress,
-	Finished,
-	Rejected
-}
--- Microservices/OrchestrationMS4/src/main/java/fr/insa/mas/orchestrationMS/Status.java
+++
@@ -1,9 +0,0 @@
-package fr.insa.mas.orchestrationMS;
-
-public enum Status {
-	WaitingForValidator,
-	WaitingForVolunteer,
-	InProgress,
-	Finished,
-	Rejected
-}
